b58d7b78e957a789fc76acebaf81a54f1620cf7c
jonathan vergara <jonathanvergararuiz@gmail.com>
2026-08-26 23:40:25 -0500

    🐛 FIX: agregando version de payload al lambda

--- terraform/api_lambdas.tf
+++ terraform/api_lambdas.tf
@@ -76,6 +76,7 @@ resource "aws_apigatewayv2_integration" "lambda_integration" {
   api_id           = aws_apigatewayv2_api.http_api.id
   integration_type = "AWS_PROXY"
   integration_uri  = aws_lambda_function.api_handler.invoke_arn
+  payload_format_version = "2.0"
 }
 
 # --- 5. Autorizador de Cognito ---
